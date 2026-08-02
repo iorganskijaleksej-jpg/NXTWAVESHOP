59de526ca75615ff183ee1bf58659bffdfd925a9
iorganskijaleksej-jpg <iorganskijaleksej@gmail.com>
2026-08-03 01:00:35 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,213 @@
+const products = [
+  {
+    id: 1,
+    images: [
+      "images/Dunk/dunk1.webP",
+      "images/Dunk/dunk2.webP",
+      "images/Dunk/dunk3.webP",
+    ],
+    price: 100,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 2,
+    images: [
+      "images/Dunk2/dunk1(2).webP",
+      "images/Dunk2/dunk2(2).webP",
+    ],
+    price: 100,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 3,
+    images: [
+      "images/Force/force1(3).webP",
+      "images/Force/force2(3).webP",
+      "images/Force/force3(3).webP",
+    ],
+    price: 100,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 4,
+    images: [
+      "images/Force2/force1(4).webP",
+      "images/Force2/force2(4).webP",
+      "images/Force2/force3(4).webP",
+    ],
+    price: 100,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 5,
+    images: [
+      "images/RO/ro1.webP",
+      "images/RO/ro2.webP",
+      "images/RO/ro3.webP",
+    ],
+    price: 125,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 6,
+    images: [
+      "images/ro2/ro1(2).webP",
+      "images/ro2/ro2(2).webP",
+      "images/ro2/ro3(2).webP",
+    ],
+    price: 135,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 7,
+    images: [
+      "images/RO3/ro1(3).webP",
+      "images/RO3/ro2(3).webP",
+      "images/RO3/ro3(3).webP",
+    ],
+    price: 130,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 8,
+    images: [
+      "images/RO4/ro1(4).webP",
+      "images/RO4/ro2(4).webP",
+      "images/RO4/ro3(4).webP",
+    ],
+    price: 135,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 9,
+    images: [
+      "images/RO5/ro1(5).webP",
+      "images/RO5/ro2(5).webP",
+      "images/RO5/ro3(5).webP",
+    ],
+    price: 110,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 10,
+    images: [
+      "images/BLNG/1/0cf48f55.webP",
+      "images/BLNG/1/fa19440a.webP",
+      "images/BLNG/1/f1972906.webP",
+    ],
+    price: 135,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 11,
+    images: [
+      "images/BLNG/2/e4b16174.webP",
+      "images/BLNG/2/db89423c.webP",
+      "images/BLNG/2/61245896.webP",
+    ],
+    price: 160,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 12,
+    images: [
+      "images/BLNG/3/bbgg.webP",
+      "images/BLNG/3/bbg.webP",
+    ],
+    price: 135,
+    sizes: [40, 41, 42, 43, 44, 45],
+  },
+  {
+    id: 13,
+    images: [
+      "images/BLNG/3/bbll.webP",
+      "images/BLNG/3/bbl.webP",
+    ],
+    price: 110,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  },
+  {
+    id: 14,
+    images: [
+      "images/BLNG/3/bbcc.webP",
+      "images/BLNG/3/bbc.webP",
+    ],
+    price: 115,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  },
+  {
+    id: 15,
+    images: [
+      "images/BLNG/3/pppp.webP",
+      "images/BLNG/3/ppp.webP",
+    ],
+    price: 115,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  }, 
+  {
+    id: 16,
+    images: [
+      "images/Sgoes/mm_1_1 (1).webP",
+    ],
+    price: 95,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  }, 
+  {
+    id: 17,
+    images: [
+      "images/Sgoes/mm_2_1 (1).webP",
+    ],
+    price: 70,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  },
+  {
+    id: 18,
+    images: [
+      "images/Sgoes/bp_s_1_1 (1).webP",
+    ],
+    price: 70,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  },
+  {
+    id: 19,
+    images: [
+      "images/Sgoes/adidas_1_1 (1).webP",
+    ],
+    price: 70,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  },
+  {
+    id: 20,
+    images: [
+      "images/Sgoes/raf_1_1 (1).webP",
+    ],
+    price: 85,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  },
+  {
+    id: 21,
+    images: [
+      "images/Sgoes/raf_2_1 (1).webP",
+    ],
+    price: 85,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  },
+  {
+    id: 22,
+    images: [
+      "images/Sgoes/tl_1_1 (1).webP",
+    ],
+    price: 95,
+    sizes: [40, 41, 42, 43, 44, 45],
+    isCatalogOnly: true
+  },
+];
